3d5ee8f7439eda731acc00a4e7bc8df374f74c58
Heinrich van Vught <heinrich.van.vught@gmail.com>
2019-07-26 11:27:58 +0100

    Minor tweaks to show date and time in logs and responses. Added a simple middleware component.

--- hello-nest/src/app.module.ts
+++ hello-nest/src/app.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
+
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, PostsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // see https://docs.nestjs.com/middleware
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
--- hello-nest/src/main.ts
+++ hello-nest/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // tslint:disable-next-line:no-console
+  console.log(`${new Date().toISOString()} App listening on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
